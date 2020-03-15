64fc2a6401fce187b4b7d5f330cdf5e33ff3255f
Trumand <furtado.pedropaulo@gmail.com>
2020-03-15 01:48:27 -0300

    Finished calculation conversion

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 import AppComponent from './components/AppComponent'
 
 export default class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
-      value: '',
+      valueBin: '',
       valueDec: ''
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+
   }
+
+  handleChange(e){
+    this.setState({
+      valueBin: e.target.value
+    })
+  }
+  handleClick(){
+    const regex = new RegExp('[a-zA-Z2-9]','gi')
+    const binarios = this.state.valueBin.match(regex)
+    if(binarios !== null){
+      return alert("Entre com um valor binário");
+    }
+    this.setState({
+      valueDec: this.state.valueBin.split('').reverse().reduce(function(acc,atl, index){
+        return acc + atl*2**index;
+      },0)
+    })
+  }
+
     
   render(){
     return(
-      <AppComponent />
+      <AppComponent
+      valueInputDec={this.state.valueDec} 
+      handleChange={this.handleChange}
+      handleClick={this.handleClick} />
     )
   }
 }
